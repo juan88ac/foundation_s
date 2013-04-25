628d1900f3ca2546122b2c186bf8c4bff012f20a
Pancho <lonchbox@gmail.com>
2013-04-25 18:42:54 +0200

    adding foundation styles

--- footer.php
+++ footer.php
@@ -10,15 +10,21 @@
 
 	</div><!-- #main -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+	<footer id="colophon" class="site-footer row" role="contentinfo">
+		<div class="site-info large-12">
 			<?php do_action( '_s_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://33themes.com/" rel="designer">33themes</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
+
+  <script>
+  document.write('<script src=' +
+  ('__proto__' in {} ? 'js/vendor/zepto' : 'js/vendor/jquery') +
+  '.js><\/script>')
+  </script>
 
 <?php wp_footer(); ?>
 
--- header.php
+++ header.php
@@ -24,18 +24,34 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
-	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
+	<header id="masthead" class="site-header row" role="banner">
+		<hgroup class="large-12">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</hgroup>
 
-		<nav id="site-navigation" class="navigation-main" role="navigation">
-			<h1 class="menu-toggle"><?php _e( 'Menu', '_s' ); ?></h1>
-			<div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', '_s' ); ?></a></div>
+		<nav id="site-navigation" class="navigation-main top-bar" role="navigation">
+          <ul class="title-area">
+            <!-- Title Area -->
+            <li class="name">
+	            <h1 class="menu-toggle"><?php _e( 'Menu', '_s' ); ?></h1>
+	            <!-- <div class="screen-reader-text skip-link"><a href="#content" title="<?php esc_attr_e( 'Skip to content', '_s' ); ?>"><?php _e( 'Skip to content', '_s' ); ?></a></div> -->
+            </li>
+            <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
+            <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
+          </ul>
 
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+	        <section class="top-bar-section">
+				<?php wp_nav_menu( array( 
+					'theme_location'=> 'primary',
+					'menu_class'	=> 'left',
+					'container'		=> '',
+					'container_class'	=> ''
+				) ); ?>
+	        </section>
+			
 		</nav><!-- #site-navigation -->
+
 	</header><!-- #masthead -->
 
-	<div id="main" class="site-main">
+	<div id="main" class="site-main row">

76e017bffa9ae3126995a077beaf91a5bc551827
Pancho <lonchbox@gmail.com>
2013-05-11 22:35:03 +0200

    WP_Nav_Menu_Widget side-nav enable

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
 	
 	/* WORDPRESS NAV-BAR SUPPORT ------------- */
-	/* Adds support for the nav-bar with flyouts in WordPress */	
+	/* Adds support for the top-bar with flyouts in WordPress */	
 	
 	$('.top-bar-section li').has('ul').addClass("has-dropdown");
 	$('.top-bar-section li ul').addClass("dropdown");
+
+	/* WORDPRESS WP_Nav_Menu_Widget SUPPORT ------------- */
+	/* Adds support for the side-nav styles in wordpress menu widget */	
+
+	$('aside.widget_nav_menu ul.menu').addClass("side-nav");
+	$('aside.widget_nav_menu ul.menu li.current-menu-item').addClass("active");	
 
 });

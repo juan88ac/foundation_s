9c7a242244a07d30ef513460f134360193343e13
Pancho <lonchbox@gmail.com>
2013-05-11 22:13:50 +0200

    register wp_nav_menu widget

--- inc/hacks.php
+++ inc/hacks.php
@@ -148,7 +148,7 @@ function unregister_default_wp_widgets() {
   //unregister_widget('WP_Widget_Recent_Comments');
   //unregister_widget('WP_Widget_RSS');
   //unregister_widget('WP_Widget_Tag_Cloud');
-  unregister_widget('WP_Nav_Menu_Widget');
+  //unregister_widget('WP_Nav_Menu_Widget');
 }
 add_action('widgets_init', 'unregister_default_wp_widgets', 1);
 

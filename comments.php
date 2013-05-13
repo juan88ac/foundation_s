c6bbe67a10af1d55fe4b7983b8f3968fdbd0ab17
Pancho <lonchbox@gmail.com>
2013-05-13 10:34:48 +0200

    custom comment form
    calendar icon on date info

--- comments.php
+++ comments.php
@@ -27,17 +27,23 @@
 
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
+			<span class="radius secondary label">
 			<?php
 				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', '_s' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
+			</span>
 		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-above" class="navigation-comment" role="navigation">
 			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', '_s' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', '_s' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', '_s' ) ); ?></div>
+			<div class="pagination-centered">
+			  <ul class="pagination">
+			    <li class="arrow"><?php previous_comments_link( __( '&larr; Older Comments', '_s' ) ); ?></li>
+				<li class="arrow"><?php next_comments_link( __( 'Newer Comments &rarr;', '_s' ) ); ?></li>
+			  </ul>
+			</div>
 		</nav><!-- #comment-nav-before -->
 		<?php endif; // check for comment navigation ?>
 
@@ -56,8 +62,12 @@
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav-below" class="navigation-comment" role="navigation">
 			<h1 class="screen-reader-text"><?php _e( 'Comment navigation', '_s' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', '_s' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', '_s' ) ); ?></div>
+			<div class="pagination-centered">
+			  <ul class="pagination">
+			    <li class="arrow"><?php previous_comments_link( __( '&larr; Older Comments', '_s' ) ); ?></li>
+				<li class="arrow"><?php next_comments_link( __( 'Newer Comments &rarr;', '_s' ) ); ?></li>
+			  </ul>
+			</div>
 		</nav><!-- #comment-nav-below -->
 		<?php endif; // check for comment navigation ?>
 
@@ -67,9 +77,61 @@
 		// If comments are closed and there are comments, let's leave a little note, shall we?
 		if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="no-comments"><?php _e( 'Comments are closed.', '_s' ); ?></p>
+		<p class="no-comments"><span class="radius secondary label"><?php _e( 'Comments are closed.', '_s' ); ?></span></p>
 	<?php endif; ?>
 
-	<?php comment_form(); ?>
+	<div class="panel">
+	<?php 
+	$comments_args= array(
 
+    'fields' => apply_filters(
+        'comment_form_default_fields', array(
+            'author' => 
+            '<div class="comment-form-author row collapse">' . 
+                '<div class="small-3 columns">
+                	<span class="prefix radius">
+                		<label for="author">' . __( 'Your Name', '_s') . '
+                	' . ( $req ? '<strong class="required">*</strong></label>
+                	</span>' : '' ) . 
+                '</div>' .
+            	'<div class="small-9 columns">
+            		<input id="author" placeholder="Your Name (No Keywords)" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />
+            	</div>'.
+            '</div>'
+                ,
+            'email'  =>
+            '<div class="comment-form-email row collapse">' . 
+                '<div class="small-3 columns">
+                	<span class="prefix radius">
+                		<label for="email">' . __( 'Your Email' ) . '
+                	' . ( $req ? '<strong class="required">*</strong></label> 
+                	</span>' : '' ) .
+                '</div>' .
+            	'<div class="small-9 columns">
+            		<input id="email" placeholder="your-real-email@example.com" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+                '" size="30"' . $aria_req . ' />
+                </div>' .
+            '</div>',
+            'url' => 
+            '<div class="comment-form-url row collapse">' .
+    			'<label class="hide" for="url">' . __( 'Website', '_s' ) . '</label>' .            
+				'<div class="small-3 large-2 columns">				
+					<span class="prefix radius">http://</span>
+				</div>' .
+				'<div class="small-9 large-10 columns">
+					<input id="url" name="url" placeholder="http://your-site-name.com" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30" /> 
+				</div>
+            </div>'
+        )
+    ),
+    'comment_field' => 
+    '<div class="comment-form-comment">' .
+    	'<label for="comment">' . __( 'Let us know what you have to say:', '_s' ) . '</label>' .
+        '<textarea class="large-12 columns" id="comment" name="comment" placeholder="' . __( 'Express your thoughts, idea or write a feedback by clicking here & start an awesome comment', '_s') . '" aria-required="true"></textarea>' .
+    '</div>',
+    'comment_notes_after' => '',
+    'title_reply' => '<h5>' . __( 'Please Post Your Comments & Reviews', '_s') . '</h5>'
+);
+	comment_form($comments_args, $post_id);?>
+	</div>
 </div><!-- #comments -->
--- inc/template-tags.php
+++ inc/template-tags.php
@@ -32,24 +32,27 @@ function _s_content_nav( $nav_id ) {
 	?>
 	<nav role="navigation" id="<?php echo esc_attr( $nav_id ); ?>" class="<?php echo $nav_class; ?>">
 		<h1 class="screen-reader-text"><?php _e( 'Post navigation', '_s' ); ?></h1>
+		<div class="pagination-centered">
+		  <ul class="pagination">
 
 	<?php if ( is_single() ) : // navigation links for single posts ?>
 
-		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', '_s' ) . '</span> %title' ); ?>
-		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', '_s' ) . '</span>' ); ?>
+		<li class="arrow"><?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', '_s' ) . '</span> %title' ); ?></li>
+		<li class="arrow"><?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', '_s' ) . '</span>' ); ?></li>
 
 	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
 
 		<?php if ( get_next_posts_link() ) : ?>
-		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '_s' ) ); ?></div>
+		<li class="arrow"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', '_s' ) ); ?></li>
 		<?php endif; ?>
 
 		<?php if ( get_previous_posts_link() ) : ?>
-		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '_s' ) ); ?></div>
+		<li class="arrow"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', '_s' ) ); ?></li>
 		<?php endif; ?>
 
 	<?php endif; ?>
-
+		  </ul>
+		</div>
 	</nav><!-- #<?php echo esc_html( $nav_id ); ?> -->
 	<?php
 }
@@ -67,7 +70,7 @@ function _s_comment( $comment, $args, $depth ) {
 		case 'pingback' :
 		case 'trackback' :
 	?>
-	<li class="post pingback">
+	<li class="post pingback panel">
 		<p><?php _e( 'Pingback:', '_s' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __( 'Edit', '_s' ), '<span class="edit-link">', '<span>' ); ?></p>
 	<?php
 			break;
@@ -75,23 +78,26 @@ function _s_comment( $comment, $args, $depth ) {
 	?>
 	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
 		<article id="comment-<?php comment_ID(); ?>" class="comment panel">
+		
 			<footer class="row">
 				<div class="comment-author">
 					<div class="large-2 columns"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" class="th radius"><?php echo get_avatar( $comment, 74 ); ?></a></div>
 					<span class="large-10 columns"><?php printf( __( '%s <span class="says">says:</span>', '_s' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?></span>
 				</div><!-- .comment-author .vcard -->
-				<?php if ( $comment->comment_approved == '0' ) : ?>
-					<em><?php _e( 'Your comment is awaiting moderation.', '_s' ); ?></em>
-					<br />
-				<?php endif; ?>
-
 				<div class="comment-meta commentmetadata large-10 columns">
 					<a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>"><time datetime="<?php comment_time( 'c' ); ?>">
 					<?php printf( _x( '%1$s at %2$s', '1: date, 2: time', '_s' ), get_comment_date(), get_comment_time() ); ?>
 					</time></a>
-					<?php edit_comment_link( __( 'Edit', '_s' ), '<span class="edit-link">', '<span>' ); ?>
+					<?php edit_comment_link( __( 'Edit', '_s' ), '<span class="tiny button round edit-link">', '<span>' ); ?>
 				</div><!-- .comment-meta .commentmetadata -->
 			</footer>
+
+			<?php if ( $comment->comment_approved == '0' ) : ?>
+			<div data-alert class="alert-box secondary">
+				<?php _e( 'Your comment is awaiting moderation.', '_s' ); ?>
+				<a href="#" class="close">&times;</a>
+			</div>
+			<?php endif; ?>
 
 			<div class="comment-content"><?php comment_text(); ?></div>
 
@@ -112,7 +118,14 @@ if ( ! function_exists( '_s_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function _s_posted_on() {
-	printf( __( 'Posted on <a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a><!--<span class="byline"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>-->', '_s' ),
+	printf( __( '
+	<span aria-hidden="true" class="icon-calendar"></span>
+	<span class="radius secondary label">Posted on</span> 
+	<a href="%1$s" title="%2$s" rel="bookmark">
+		<time class="entry-date" datetime="%3$s">%4$s</time>
+	</a>
+	<span class="byline hide"> by <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>
+	', '_s' ),
 		esc_url( get_permalink() ),
 		esc_attr( get_the_time() ),
 		esc_attr( get_the_date( 'c' ) ),

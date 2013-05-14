5a2cff4408e83b24fb58bb985d8a6fc3d041527c
Pancho <lonchbox@gmail.com>
2013-05-15 00:51:38 +0200

    add conditional statement for image size
    Especial condition to use different image size for page slider template.

--- partials/content-slider.php
+++ partials/content-slider.php
@@ -12,7 +12,11 @@
 	<ul data-orbit>
 		<?php while ($orbit_slider->have_posts()) : $orbit_slider->the_post(); ?>
 		  <li>
-			<?php the_post_thumbnail('slider'); ?>
+		  	<?php if(is_page_template('template-slider.php')): ?>
+				<?php the_post_thumbnail('template-slider'); ?>
+		  	<?php else: ?>
+				<?php the_post_thumbnail('slider'); ?>
+			<?php endif; ?>
 		    <div class="orbit-caption">
 			    <?php the_excerpt(); ?>
 		    </div>
